69cff28f9dbe0670643383dc69323cae33aad920
Marius Hambro Bovbjerg <marius@hambro.eu>
2021-04-22 15:23:47 +0200

    Login and roles gaming :)

---
+++ GUI_Hotel/Data/DbEmployee.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace GUI_Hotel.Data
+{
+    public class DbEmployee
+    {
+        public static List<string> emails = new List<string> { "Kitchen@HotelHost", "Reception@HotelHost", "Restaurant@HotelHost" };
+        public static List<string> passwords = new List<string> { "Kitchen123/", "Reception123/", "Restaurant123/" };
+        public static List<Tuple<string, string>> claims = new List<Tuple<string, string>> { new("Kitchen", "KITCHEN"), new("Reception", "RECEPTION"), new("Restaurant", "RESTAURANT") };
+
+        public static void CreateKitchenEmployee(UserManager<IdentityUser> userManager, ILogger log)
+        {
+
+            if (userManager.FindByNameAsync(emails[0]).Result == null)
+            {
+                log.LogWarning("Seeding the " + emails[0] + " user");
+                var user = new IdentityUser
+                {
+                    UserName = emails[0],
+                    Email = emails[0]
+                };
+                IdentityResult result = userManager.CreateAsync
+                    (user, passwords[0]).Result;
+                if (result.Succeeded)
+                {
+                    var claim = new Claim(claims[0].Item1, claims[0].Item2);
+                    userManager.AddClaimAsync(user, claim);
+                }
+
+            }
+
+        }
+        
+        public static void CreateReceptionEmployee(UserManager<IdentityUser> userManager, ILogger log)
+        {
+
+            if (userManager.FindByNameAsync(emails[1]).Result == null)
+            {
+                log.LogWarning("Seeding the " + emails[1] + " user");
+                var user = new IdentityUser
+                {
+                    UserName = emails[1],
+                    Email = emails[1]
+                };
+                IdentityResult result = userManager.CreateAsync
+                    (user, passwords[1]).Result;
+                if (result.Succeeded)
+                {
+                    var claim = new Claim(claims[1].Item1, claims[1].Item2);
+                    userManager.AddClaimAsync(user, claim);
+                }
+
+            }
+
+        }
+
+        public static void CreateRestaurantEmployee(UserManager<IdentityUser> userManager, ILogger log)
+        {
+
+            if (userManager.FindByNameAsync(emails[2]).Result == null)
+            {
+                log.LogWarning("Seeding the " + emails[2] + " user");
+                var user = new IdentityUser
+                {
+                    UserName = emails[2],
+                    Email = emails[2]
+                };
+                IdentityResult result = userManager.CreateAsync
+                    (user, passwords[2]).Result;
+                if (result.Succeeded)
+                {
+                    var claim = new Claim(claims[2].Item1, claims[2].Item2);
+                    userManager.AddClaimAsync(user, claim);
+                }
+
+            }
+
+        }
+    }
+}
--- GUI_Hotel/Startup.cs
+++ GUI_Hotel/Startup.cs
@@ -1,17 +1,13 @@
 using GUI_Hotel.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using System.Threading;
 
 namespace GUI_Hotel
 {
@@ -28,17 +24,50 @@ namespace GUI_Hotel
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                options.UseSqlServer("server=[::1],1433; User Id=sa; Password=password_123; database=GuiHotelDb; trusted_connection=false;"));
             services.AddDatabaseDeveloperPageExceptionFilter();
 
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddDefaultIdentity<IdentityUser>(options => {
+                options.SignIn.RequireConfirmedAccount = false;
+
+                // Password Settings
+                options.Password.RequiredLength = 8;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredUniqueChars = 4;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireDigit = true;
+
+                // User settings
+                options.User.AllowedUserNameCharacters =
+                    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+;:";
+                options.User.RequireUniqueEmail = false;
+
+            }).AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy(
+                    "IsKitchen",
+                    policyBuilder => policyBuilder
+                     .RequireClaim("Kitchen"));
+                options.AddPolicy(
+                    "IsReception",
+                    policyBuilder => policyBuilder
+                     .RequireClaim("Reception"));
+                options.AddPolicy(
+                    "IsRestaurant",
+                    policyBuilder => policyBuilder
+                     .RequireClaim("Restaurant"));
+            });
+
+
             services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env,
+            UserManager<IdentityUser> userManager, ILogger<Startup> log)
         {
             if (env.IsDevelopment())
             {
@@ -58,7 +87,18 @@ namespace GUI_Hotel
 
             app.UseAuthentication();
             app.UseAuthorization();
-
+            
+            if (userManager.FindByNameAsync("Kitchen@HotelHost").Result == null ||
+                userManager.FindByNameAsync("Reception@HotelHost").Result == null ||
+                userManager.FindByNameAsync("Restaurant@HotelHost").Result == null)
+            {
+                DbEmployee.CreateKitchenEmployee(userManager, log);
+                Thread.Sleep(200);
+                DbEmployee.CreateReceptionEmployee(userManager, log);
+                Thread.Sleep(200);
+                DbEmployee.CreateRestaurantEmployee(userManager, log);
+            }
+            
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
