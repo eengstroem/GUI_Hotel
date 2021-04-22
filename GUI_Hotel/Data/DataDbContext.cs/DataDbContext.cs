f1efd341a95daeb183a73275bea2431604a99b45
eengstroem <59916483+eengstroem@users.noreply.github.com>
2021-04-22 16:04:44 +0200

    added context for data

---
+++ GUI_Hotel/Data/DataDbContext.cs/DataDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GUI_Hotel.Data.DataDbContext.cs
+{
+    public class DataDbContext : DbContext
+    {
+        public DataDbContext(DbContextOptions<DataDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
