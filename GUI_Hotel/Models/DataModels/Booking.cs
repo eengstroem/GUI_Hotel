cccd70f4b3bfeb331d57a31fff9806a5d18477b4
eengstroem <59916483+eengstroem@users.noreply.github.com>
2021-04-24 15:49:00 +0200

    new booking class

--- GUI_Hotel/Models/DataModels/Booking.cs
+++ GUI_Hotel/Models/DataModels/Booking.cs
@@ -1,22 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
-namespace GUIHotel.Models.DataModels
+namespace GUI_Hotel.Models.DataModels
 {
     public class Booking
     {
+
+
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int BookingId{get;set;}
+        public int BookingId { get; set; }
 
         [Required]
         [DataType(DataType.Date)]
         public DateTime BookingDate { get; set; }
 
-        public ICollection<Room> BookedRooms { get; set; }
+        public int RoomNumber { get; set; }
+
+
+        public int AdultsBooked { get; set; }
+
+        public int AdultsCheckedIn { get; set; }
+
+        public int ChildrenBooked { get; set; }
+
+        public int ChildrenCheckedIn { get; set; }
+
+
+
+
     }
 }
