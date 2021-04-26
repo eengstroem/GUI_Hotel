acf0d75aed2d31308326385375a7151683ca842d
KunJakob <jakobhkristensen@gmail.com>
2021-04-26 15:07:04 +0200

    tilføjet kitchen

--- GUI_Hotel/Controllers/KitchenController.cs
+++ GUI_Hotel/Controllers/KitchenController.cs
@@ -8,6 +8,9 @@ using Microsoft.EntityFrameworkCore;
 using GUI_Hotel.Data;
 using GUI_Hotel.Models.DataModels;
 using Microsoft.AspNetCore.Authorization;
+using GUI_Hotel.Models;
+using GUI_Hotel.ViewModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace GUI_Hotel.Controllers
 {
@@ -21,142 +24,26 @@ namespace GUI_Hotel.Controllers
         }
 
         // GET: Kitchen
-        [Authorize(Policy = "IsReception")]
-        public async Task<IActionResult> Index()
+        [Authorize(Policy = "IsKitchen")]
+        public IActionResult Index()
         {
-            return View(await _context.Bookings.ToListAsync());
-        }
 
-        // GET: Kitchen/Details/5
-        [Authorize(Policy = "IsReception")]
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var booking = await _context.Bookings
-                .FirstOrDefaultAsync(m => m.BookingId == id);
-            if (booking == null)
-            {
-                return NotFound();
-            }
-
-            return View(booking);
-        }
-
-        // GET: Kitchen/Create
-        [Authorize(Policy = "IsReception")]
-        public IActionResult Create()
-        {
             return View();
         }
 
-        // POST: Kitchen/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        [Authorize(Policy = "IsReception")]
-        public async Task<IActionResult> Create([Bind("BookingId,BookingDate,RoomNumber,AdultsBooked,AdultsCheckedIn,ChildrenBooked,ChildrenCheckedIn")] Booking booking)
+        [Authorize(Policy = "IsKitchen")]
+        public IActionResult Find(DateTime Date)
         {
-            if (ModelState.IsValid)
+
+            var filterDate = Date.Date.AddDays(1);
+            var res = _context.Bookings.Where(b => b.BookingDate >= Date.Date && b.BookingDate < filterDate).ToList();
+            var vm = new KitchenViewModel()
             {
-                _context.Add(booking);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(booking);
-        }
+                Bookings = res,
+                Date = Date.Date,
+            };
+            return View(vm);
 
-        // GET: Kitchen/Edit/5
-        [Authorize(Policy = "IsReception")]
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var booking = await _context.Bookings.FindAsync(id);
-            if (booking == null)
-            {
-                return NotFound();
-            }
-            return View(booking);
-        }
-
-        // POST: Kitchen/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        [Authorize(Policy = "IsReception")]
-        public async Task<IActionResult> Edit(int id, [Bind("BookingId,BookingDate,RoomNumber,AdultsBooked,AdultsCheckedIn,ChildrenBooked,ChildrenCheckedIn")] Booking booking)
-        {
-            if (id != booking.BookingId)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(booking);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!BookingExists(booking.BookingId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(booking);
-        }
-
-        // GET: Kitchen/Delete/5
-        [Authorize(Policy = "IsReception")]
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var booking = await _context.Bookings
-                .FirstOrDefaultAsync(m => m.BookingId == id);
-            if (booking == null)
-            {
-                return NotFound();
-            }
-
-            return View(booking);
-        }
-
-        // POST: Kitchen/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        [Authorize(Policy = "IsReception")]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var booking = await _context.Bookings.FindAsync(id);
-            _context.Bookings.Remove(booking);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool BookingExists(int id)
-        {
-            return _context.Bookings.Any(e => e.BookingId == id);
         }
     }
 }
---
+++ GUI_Hotel/ViewModel/KitchenViewModel.cs
@@ -0,0 +1,31 @@
+﻿using GUI_Hotel.Models;
+using GUI_Hotel.Models.DataModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GUI_Hotel.ViewModel
+{
+    public class KitchenViewModel
+    {
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+
+        public IEnumerable<Booking> Bookings { get; set; }
+        public int CheckedInChildren { get => Bookings.Sum(b => b.ChildrenCheckedIn); }
+        public int CheckedInAdults { get => Bookings.Sum(b => b.AdultsCheckedIn); }
+        public int BookedChildren { get => Bookings.Sum(b => b.ChildrenBooked); }
+        public int BookedAdults { get => Bookings.Sum(b => b.AdultsBooked); }
+
+        public int ExpectedGuests { get => BookedAdults + BookedChildren; }
+        public int GuestsNotCheckedIn { get => BookedAdults + BookedChildren - CheckedInChildren - CheckedInAdults; }
+
+        public int ChildrenNotCheckedIn { get => BookedChildren - CheckedInChildren; }
+        public int AdultsNotCheckedIn { get => BookedAdults - CheckedInAdults; }
+
+
+
+    }
+}
